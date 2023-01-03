aee9a0aa44c9d50db7cca3d91f4803fc69318c97
DevJihwan <jh552016@naver.com>
2023-01-04 08:59:49 +0900

    chap 3 ing..

--- Chap3/set.js
+++ Chap3/set.js
@@ -1,17 +1,15 @@
-const userIds = [
-    1,2,1,3
-]
+const userIds = [1, 2, 1, 3];
 
 const uniqueIds = new Set(userIds);
+
 console.log(uniqueIds);
 
 uniqueIds.add(10);
-console.log('add 10', uniqueIds);
+console.log(uniqueIds);
 
-console.log('has', uniqueIds.has(3));
-
-console.log('size', uniqueIds.size);
+console.log("has", uniqueIds.has(3));
+console.log("size", uniqueIds.size);
 
 for (let item of uniqueIds) {
-    console.log('iterate', item);
+  console.log("iterate", item);
 }
